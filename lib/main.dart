5108086bb33d8c67e0e7f79fb4439f35496ae904
Gabriel <gvsilva7767@gmail.com>
2023-11-16 17:30:14 -0400

    Projeto Final- Bio_Link

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
+
 void main() {
   runApp(MyApp());
 }
@@ -22,31 +23,63 @@ class MyApp extends StatelessWidget {
 class MyWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        CircleAvatar(
-          radius: 100,
-          backgroundImage: AssetImage('lib/assets/frontal.jpg'),
+    return Center(
+      child: Container(
+        width: MediaQuery.of(context).size.width * 0.4, // Este é o tamanho do container em relação à largura da tela
+        height: MediaQuery.of(context).size.height * 0.8, // Este é o tamanho do container em relação à altura da tela
+        padding: EdgeInsets.all(20), // Adiciona um padding ao container
+        decoration: BoxDecoration(
+          color: Color.fromARGB(255, 239, 236, 236), // Esta é a cor de fundo do retângulo
+          borderRadius: BorderRadius.circular(10), // Isso faz com que os cantos do retângulo sejam arredondados
         ),
-        ElevatedButton(
-          onPressed: () => launch('https://www.link1.com'),
-          child: Text('Instagram'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CircleAvatar(
+              radius: 150, // Aumenta o raio do CircleAvatar
+              backgroundImage: AssetImage('../lib/assets/frontal1.jpg'),
+            ),
+            Text('FASTECH - Faculdade de Tecnologia de Sinop', style: TextStyle(fontSize: 14)), // Esta é a legenda
+            SizedBox(height: 20), // Adiciona um espaço entre o avatar e os botões
+            ButtonTheme(
+              minWidth: 300.0, // Aumenta a largura mínima dos botões
+              height: 60.0, // Aumenta a altura dos botões
+              child: Column(
+                children: [
+                  ElevatedButton(
+                    onPressed: () => _launchURL('https://www.instagram.com/fastech.sinop/'),
+                    child: Text('Instagram', style: TextStyle(fontSize: 20)), // Aumenta o tamanho da fonte do texto do botão
+                  ),
+                  SizedBox(height: 20), // Adiciona um espaço entre os botões
+                  ElevatedButton(
+                    onPressed: () => _launchURL('https://www.facebook.com/Fastech.Sinop/?locale=pt_BR'),
+                    child: Text('Facebook', style: TextStyle(fontSize: 20)), // Aumenta o tamanho da fonte do texto do botão
+                  ),
+                  SizedBox(height: 20), // Adiciona um espaço entre os botões
+                  ElevatedButton(
+                    onPressed: () => _launchURL('https://www.linkedin.com/company/fastech-sinop/'),
+                    child: Text('Linkedin', style: TextStyle(fontSize: 20)), // Aumenta o tamanho da fonte do texto do botão
+                  ),
+                  SizedBox(height: 20), // Adiciona um espaço entre os botões
+                  ElevatedButton(
+                    onPressed: () => _launchURL('https://www.faculdadefastech.com.br/'),
+                    child: Text('Site', style: TextStyle(fontSize: 20)), // Aumenta o tamanho da fonte do texto do botão
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
-        ElevatedButton(
-          onPressed: () => launch('https://www.link2.com'),
-          child: Text('Link 2'),
-        ),
-        ElevatedButton(
-          onPressed: () => launch('https://www.link3.com'),
-          child: Text('Link 3'),
-        ),
-        ElevatedButton(
-          onPressed: () => launch('https://www.link4.com'),
-          child: Text('Link 4'),
-        ),
-      ],
+      ),
     );
   }
-}
 
+  void _launchURL(String urlString) async {
+    var url = Uri.parse(urlString);
+    if (await canLaunchUrl(url)) {
+      await launchUrl(url);
+    } else {
+      throw 'Não foi possível abrir $urlString';
+    }
+  }
+}
